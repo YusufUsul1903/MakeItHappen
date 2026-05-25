aa618707fe94b1a85bf8ce8ae063058bba5c4cb4
Yusuf Usul <jj.starlord12@gmail.com>
2026-05-25 12:12:14 +0200

    sidebar toegevoegd

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,12 +1,16 @@
 export const getDashboard = (req, res) => {
     const tasks = [
-        { title: 'Website stylen', completed: false },
-        { title: 'Database opzetten', completed: false },
-        { title: 'EJS implementeren', completed: true }
+        { _id: '1', title: 'Website stylen', completed: false },
+        { _id: '2', title: 'Database opzetten', completed: false },
+        { _id: '3', title: 'EJS implementeren', completed: true }
     ];
 
+    const categories = [];
+
     res.render('pages/index', {
-        title: 'Make It Happen - Dashboard',
-        tasks: tasks 
+        title: 'Nudge — Dashboard',
+        tasks: tasks,
+        categories: categories,
+        activeCategoryId: null
     });
 };
